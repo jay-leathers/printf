7f44efc8463e2ba4a4059bcf87ed7e6af1318183
Odeleke Shukurat <105014551+ShukrahOdeyemi1@users.noreply.github.com>
2022-07-12 22:19:30 +0100

    Create print_hex_big.c

---
+++ print_hex_big.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * print_hex_big - prints a number in hexadecimal base,
+ * in uppercase
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct that determines
+ * if a flag is passed to _printf
+ * Description: the function calls convert() which in turns converts the input
+ * number into the correct base and returns it as a string
+ * Return: the number of char printed
+ */
+int print_hex_big(va_list l, flags_t *f)
+{
+	unsigned int num = va_arg(l, unsigned int);
+	char *str = convert(num, 16, 0);
+	int count = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		count += _puts("0X");
+	count += _puts(str);
+	return (count);
+}
