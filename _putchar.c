e3c8966a7e05fed62015a6a610f6f1a58060d25a
jay-leathers <104981067+jay-leathers@users.noreply.github.com>
2022-07-12 22:04:18 +0100

    Create _putchar.c

---
+++ _putchar.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * _putchar uses a local buffer of 1024 to call write
+ * as little as possible
+ *
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	static char buf[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
+}
