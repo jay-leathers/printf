c19e1e091c1d6a92507679151c3eb85a6baf25ef
Odeleke Shukurat <105014551+ShukrahOdeyemi1@users.noreply.github.com>
2022-07-12 23:17:30 +0100

    Hot fix

--- print_string.c
+++ print_string.c
@@ -1,21 +1,68 @@
-#include "main.h"
-
+#include "holberton.h"
 
 /**
- * print_string - loops through a string and prints
- * every character
- * @l: va_list arguments from _printf
- * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
- * Return: number of char printed
+ *print_chars - prints a char
+ *@list: list of arguments
+ *@func: pointer to the struct flags in which we turn the flags on
+ *Return: number of characters printed.
  */
-int print_string(va_list l, flags_t *f)
+int print_chars(va_list list, param_func *func)
 {
-	char *s = va_arg(l, char *);
+	unsigned int i = 1;
+	unsigned int count = 0;
+	unsigned int str = va_arg(list, int);
 
-	(void)f;
+	if (func->minus_flag)
+		count += _putchar(str);
+	while (i++ < func->width)
+		count += _putchar(' ');
+	if (!func->minus_flag)
+		count += _putchar(str);
+	return (count);
+}
 
-	if (!s)
-		s = "(null)";
-	return (_puts(s));
+/**
+ *print_strings - prints strings
+ *@list: list of arguments
+ *@func: pointer to the struct flags in which we turn the flags on/off
+ *Return: number of characters printed
+ */
+int print_strings(va_list list, param_func *func)
+{
+	char *str;
+	unsigned int i, count, str_len, j;
+
+	i = 0;
+	count = 0;
+	str = va_arg(list, char *);
+	str_len = _strlen(str);
+	j = str_len;
+	(void)func;
+
+	switch ((int)(!str))
+		case 1:
+			str = "(null)";
+	if (func->precision < str_len)
+		j = str_len = func->precision;
+	if (func->minus_flag)
+	{
+		if (func->precision != UINT_MAX)
+			for (i = 0; i < str_len; i++)
+				count += _putchar(*str++);
+		else
+			count += _puts(str);
+	}
+	while (j++ < func->width)
+		count += _putchar(' ');
+	if (!func->minus_flag)
+	{
+		if (func->precision != UINT_MAX)
+			for (i = 0; i < str_len; i++)
+				count += _putchar(*str++);
+		else
+			count += _puts(str);
+
+	}
+	return (count);
+
 }
